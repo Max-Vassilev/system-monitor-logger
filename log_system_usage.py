1befb34c8c56ed96aeb8a2f9940cbf9fd3441b31
Maxim Vassilev <maximvassilev01@gmail.com>
2025-04-15 11:53:56 +0300

    Update log_system_usage.py

--- log_system_usage.py
+++ log_system_usage.py
@@ -13,7 +13,8 @@ def log_system_usage():
     # Get memory usage as a percentage (RAM)
     memory_usage = psutil.virtual_memory().percent
     # Get disk usage as a percentage (SSD/HDD)
-    disk_usage = psutil.disk_usage("/").percent
+    disk_usage = psutil.disk_usage("/Users").percent
+    
     # Log the results to a file
     logging.info(
         f"CPU Usage: {cpu_usage}% | Memory Usage: {memory_usage}% | Disk Usage: {disk_usage}%"
@@ -22,4 +23,4 @@ def log_system_usage():
 if __name__ == "__main__":
     while True:
         log_system_usage()
-        time.sleep(60)
+        time.sleep(600)
